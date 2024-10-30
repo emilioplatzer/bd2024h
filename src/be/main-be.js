ab7972ea2caad46167c187d02d651b74d6b80f04
Emilio Platzer <emilioplatzer@gmail.com>
2024-10-30 20:50:48 -0300

    agregamos la estructura de la db para que sea la fuente única de conocimiento sobre eso

--- src/be/main-be.js
+++ src/be/main-be.js
@@ -5,19 +5,39 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const port = 3000
 
-
 const pg = require('pg');
 const fs = require('fs').promises;
 
+const miEstructura = {
+    tablas: {
+        producto: {
+            campos: {
+                id: { tipo: 'serial', pk:true },
+                nombre: { tipo: 'text' },
+                categoria: { tipo: 'text' },
+                lugar: { tipo: 'text' },
+            }
+        },
+        lugares: {
+            campos: {
+                lugar: { tipo: 'text', pk:true },
+                nombre: { tipo: 'text' },
+                ubicacion: { tipo: 'text' },
+            }
+        }
+    }
+}
+
+
 app.get('/', (req, res) => {
     res.send(`Bienvenido al sitio de recomendaciones!    
-        <br> ver <a href='./productos'>productos</a>
-        <br> cargar nuevo <a href='./nuevo-producto'>producto</a>
+        <br> ver <a href='./producto/lista'>productos</a>
+        <br> cargar nuevo <a href='./producto/nuevo'>producto</a>
     `)
 })
 
-app.get('/nuevo-producto', async (req, res) => {
-    res.send(`<form action="/grabar-nuevo-producto" method="post">
+app.get('/producto/nuevo', async (req, res) => {
+    res.send(`<form action="/producto/grabar-nuevo" method="post">
         <label>nombre <input name="nombre"></label> <br>
         <label>categoría <input name="categoria"></label> <br>
         <label>lugar <input name="lugar"></label> <br>
@@ -47,7 +67,7 @@ async function enUnaConexionaBD(algo){
     return result;
 }
 
-app.post('/grabar-nuevo-producto', async (req, res) => {
+app.post('/producto/grabar-nuevo', async (req, res) => {
     console.log(req.body);
 
     const result = await enUnaConexionaBD(async client => {
@@ -56,14 +76,14 @@ app.post('/grabar-nuevo-producto', async (req, res) => {
             [req.body.nombre, req.body.categoria, req.body.lugar]
         );
     })
-    res.send(`grabado el producto ${result.rows[0].id}!<BR><a href='./productos'>lista de prodcutos</a>`)
+    res.send(`grabado el producto ${result.rows[0].id}!<BR><a href='../producto/lista'>lista de productos</a>`)
 })
 
 function sanarHTML(texto){
     return (texto+'').replace('&',"&amp;").replace('<',"&lt;").replace('>',"&gt;");
 }
 
-app.get('/productos', async (req, res) => {
+app.get('/producto/lista', async (req, res) => {
 
     const result = await enUnaConexionaBD(async client=>{
         return await client.query('SELECT * FROM producto')
@@ -74,10 +94,28 @@ app.get('/productos', async (req, res) => {
             <tr><th>id</th><th>nombre</th><th>categoria</th><th>lugar</th></tr>
             ${result.rows.map(row=>`<tr><td>${sanarHTML(row.id)}</td><td>${sanarHTML(row.nombre)}</td><td>${sanarHTML(row.categoria)}</td><td>${sanarHTML(row.lugar)}</td></tr>`).join('')}
         </table>
+        <a href='/producto/nuevo'>+ agregar uno</a>
     `)
 })
   
-    
+
+// TODO hay que poner un middleware para que solo lo pueda ejecutar un admin:
+app.get('/dump-db', async (req,res) => {
+    var sql = []
+    for (var tabla in miEstructura.tablas) {
+        sql.push(`create table ${tabla} (`);
+        var defTabla = miEstructura.tablas[tabla];
+        var sqlCampos = []
+        for (var campo in defTabla.campos) {
+            var defCampo = defTabla.campos[campo];
+            sqlCampos.push(`  ${campo} ${defCampo.tipo}${defCampo.pk?' primary key':''}`)
+        }
+        sql.push(sqlCampos.join(',\n'));
+        sql.push(`);`);
+    }
+    await fs.writeFile('local-dump.sql', sql.join('\n'), 'utf8');
+    res.send('Forbidensen!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
